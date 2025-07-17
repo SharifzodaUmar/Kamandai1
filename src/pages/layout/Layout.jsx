7f59ea61bcf05b3eb3db29b18965ec6ce41ffb7f
SharifzodaUmar <SharifzodaUmar>
2025-07-17 17:02:38 +0000

    Umar

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,45 +1,207 @@
-import React from 'react'
-import {Link, Outlet} from 'react-router'
-const Layout = () => {
-  return (
-    <div>
-        <header>
-            <ul className='flex items-center gap-[30px]'>
+import React, { useState } from 'react'
 
+import {Link, Outlet} from 'react-router'
+
+import i1 from '/i1.png'
+import i2 from '/i2.png'
+import i3 from '/i3.png'
+import i4 from '/i4.png'
+import lines from '/lines.png'
+import p from '/p.png'
+import tr from '/tr.png'
+import s1 from '/s1.svg'
+import s2 from '/s2.svg'
+import s3 from '/s3.svg'
+import s4 from '/s4.svg'
+import { Drawer } from "antd";
+const Layout = () => {
+    
+  let [open,setOpen]=useState(false)
+  function onClose() {
+    setOpen(false)
+  }
+  return (
+    <div className='max-w-[1388px] p-[20px] m-auto bg-[#0F0F0F] text-white'>
+
+
+
+
+
+        <header className='w-full h-[] sm:h-[89px] rounded-2xl flex items-center justify-around border border-[#1C1C21] bg-[#1A1A1A]'>
+         <p className='text-4xl font-bold'>NexGen</p>
+                <Drawer
+  title="Pages"
+  closable={true}
+  onClose={onClose}
+  open={open}
+  placement="left"
+>
+  <p className="text-[28px] font-extrabold">Pages:</p>
+  <ul className="flex flex-col text-[24px] font-bold gap-[10px]">
+
+    
 <Link to={'/'}> 
-<p>Home</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Home</p>
    </Link>
 <Link to={'service'}> 
-<p>Service</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Service</p>
    </Link>  
 <Link to={'about'}> 
-<p>About</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>About</p>
    </Link>  
 <Link to={'project'}> 
-<p>Project</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Project</p>
    </Link>  
 <Link to={'career'}> 
-<p>Сareer</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Сareer</p>
    </Link>  
 <Link to={'contact'}> 
-<p>Сontact</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Сontact</p>
    </Link>  
 <Link to={'blog1'}> 
-<p>Blog1</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Blog1</p>
    </Link>  
 <Link to={'blog2'}> 
-<p>Blog2</p>
+<p onClick={() => setOpen(false)} className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Blog2</p>
+   </Link>
+  </ul>
+</Drawer>
+            <ul className='sm:flex hidden items-center gap-[30px]'>
+
+<Link to={'/'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Home</p>
+   </Link>
+<Link to={'service'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Service</p>
+   </Link>  
+<Link to={'about'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>About</p>
+   </Link>  
+<Link to={'project'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Project</p>
+   </Link>  
+<Link to={'career'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Сareer</p>
+   </Link>  
+<Link to={'contact'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Сontact</p>
+   </Link>  
+<Link to={'blog1'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Blog1</p>
+   </Link>  
+<Link to={'blog2'}> 
+<p className='font-[500] p-[10px] bg-[#0F0F0F] rounded-xl'>Blog2</p>
    </Link>  
    
    
    </ul>
+   <img
+  className="sm:hidden border border-blue-500 rounded-2xl p-[3px]"
+  onClick={() => setOpen(true)}
+  src={lines}
+  alt="menu"
+/>
+
         </header>
         <main>
             <Outlet/>
         </main>
 
-        <footer>
+<footer className='mt-[50px]'>  
+        <div className='w-full border-2 h-[1066px] rounded-3xl border-[#1F1F1F] sm:h-[523px] my-[10px] flex-col flex items-center justify-around'>
+<article className='sm:w-[1340px] h-[50px] justify-center sm:h-[136px] bg-[#1A1A1A] sm:flex-row flex-col flex items-center sm:justify-between px-[60px] sm:px-[100px] rounded-xl'>
+<p className='sm:text-[38px] text-[28px] font-[600]'>Testimonials</p><p className='text-[#B3B3B2] font-[500] sm:block hidden'><span className='p-[10px] bg-[#1A1A1A] rounded-[50%] border border-[#262626]'>↗</span> ALL Testimonials</p>
 
+</article>
+
+
+<section className='w-full gap-[10px] items-center justify-around h-[900px] px-[10px] sm:h-[357px] grid sm:grid-cols-4 sm:grid-rows-1 '>
+<article className='sm:h-[357px] h-[290px] bg-[#1A1A1A] rounded-xl flex flex-col  items-center justify-between'>
+   <div className='p-[20px] flex flex-col gap-[30px] items-center justify-around'>  
+<p className='text-[22px] font-[500]'>NexGen turned our business around!</p>
+<p className='text-[#B3B3B2]'>Their digital marketing strategies helped us reach new customers and increase our revenue by 30% within just a few months. Highly recommended!</p></div>
+<div className='flex items-center justify-around w-full sm:h-[91px] h-[87px] rounded-br-2xl rounded-bl-2xl bg-[#1F1F1F]'>
+<img src={p} alt="" />
+<div>
+   <p className='text-[18px] font-[500]'>Sarah Thompson</p>
+   <p className='text-[#81807E]'>CEO of BlueBloom</p>
+</div>
+<p className='rounded-[50%] text-2xl border border-[#333333] px-[5px]'>→</p>
+</div>
+</article>
+<article className='sm:h-[357px] h-[290px] bg-[#1A1A1A] rounded-xl flex flex-col  items-center justify-between'>
+   <div className='p-[20px] flex flex-col gap-[30px] items-center justify-around'>  
+<p className='text-[22px] font-[500]'>NexGen turned our business around!</p>
+<p className='text-[#B3B3B2]'>Their digital marketing strategies helped us reach new customers and increase our revenue by 30% within just a few months. Highly recommended!</p></div>
+<div className='flex items-center justify-around w-full sm:h-[91px] h-[87px] rounded-br-2xl rounded-bl-2xl bg-[#1F1F1F]'>
+<img src={p} alt="" />
+<div>
+   <p className='text-[18px] font-[500]'>Sarah Thompson</p>
+   <p className='text-[#81807E]'>CEO of BlueBloom</p>
+</div>
+<p className='rounded-[50%] text-2xl border border-[#333333] px-[5px]'>→</p>
+</div>
+</article>
+<article className='sm:h-[357px] h-[290px] bg-[#1A1A1A] rounded-xl flex flex-col  items-center justify-between'>
+   <div className='p-[20px] flex flex-col gap-[30px] items-center justify-around'>  
+<p className='text-[22px] font-[500]'>NexGen turned our business around!</p>
+<p className='text-[#B3B3B2]'>Their digital marketing strategies helped us reach new customers and increase our revenue by 30% within just a few months. Highly recommended!</p></div>
+<div className='flex items-center justify-around w-full sm:h-[91px] h-[87px] rounded-br-2xl rounded-bl-2xl bg-[#1F1F1F]'>
+<img src={p} alt="" />
+<div>
+   <p className='text-[18px] font-[500]'>Sarah Thompson</p>
+   <p className='text-[#81807E]'>CEO of BlueBloom</p>
+</div>
+<p className='rounded-[50%] text-2xl border border-[#333333] px-[5px]'>→</p>
+</div>
+</article>
+<article className='sm:h-[357px] h-[290px] bg-[#1A1A1A] rounded-xl hidden sm:flex flex-col  items-center justify-between'>
+   <div className='p-[20px] flex flex-col gap-[30px] items-center justify-around'>  
+<p className='text-[22px] font-[500]'>NexGen turned our business around!</p>
+<p className='text-[#B3B3B2]'>Their digital marketing strategies helped us reach new customers and increase our revenue by 30% within just a few months. Highly recommended!</p></div>
+<div className='flex items-center justify-around w-full sm:h-[91px] h-[87px] rounded-br-2xl rounded-bl-2xl bg-[#1F1F1F]'>
+<img src={p} alt="" />
+<div>
+   <p className='text-[18px] font-[500]'>Sarah Thompson</p>
+   <p className='text-[#81807E]'>CEO of BlueBloom</p>
+</div>
+<p className='rounded-[50%] text-2xl border border-[#333333] px-[5px]'>→</p>
+</div>
+</article>
+</section>
+
+        </div>
+        <img className='sm:block hidden m-auto my-[50px]  ' src={s2} alt="" />
+        <img className='sm:hidden m-auto my-[50px]' src={s1} alt="" />
+
+
+
+<section className='w-full rounded-2xl sm:flex-row flex-col flex items-center p-[30px] gap-[50px] sm:h-[233px] bg-[#CE7D63] text-black'>
+<div>
+   <p className='sm:text-[42px] font-[500]'>Ready to Transform Your Digital Presence?</p>
+   <p className='text-[#262626]'>Take the first step towards digital success with NexGen by your side. Our team of experts is eager to craft tailored solutions that drive growth for your business.</p>
+</div>
+<p className='bg-[#0F0F0F] w-[172px] h-[50px] flex items-center justify-center rounded-2xl text-white'>Get in Touch ↗</p>
+</section>
+
+
+<div className='w-full h-[70px] bg-[#1A1A1A] px-[10px] rounded-2xl my-[30px] flex items-center justify-between  text-[20px] text-[#676665]'>
+
+   <p className='m-auto sm:m-0'>Follow Us on Social Media •</p>
+   <p className='sm:block hidden'>Follow Us on Social Media •</p>
+   <p className='sm:block hidden'>Follow Us on Social Media •</p>
+   <p className='sm:block hidden'>Follow Us on Social Media •</p>
+   <p className='sm:block hidden'>Follow Us on Social Media •</p>
+
+</div>
+
+        <img className='sm:block hidden m-auto my-[50px]  ' src={s4} alt="" />
+        <img className='sm:hidden m-auto my-[50px]' src={s3} alt="" />
+
+
+
+        
+        
         </footer>
     </div>
   )
--- src/pages/page6-7/blog2/Blog2.jsx
+++ src/pages/page6-7/blog2/Blog2.jsx
@@ -1,8 +1,189 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import s1 from '../suratUmar/s1.png'
+import s2 from '../suratUmar/s2.png'
+import s3 from '../suratUmar/s3.png'
+import s4 from '../suratUmar/s4.png'
+import s5 from '../suratUmar/s5.png'
+import s6 from '../suratUmar/s6.png'
+import social from '../suratUmar/social.png'
+
+import {useTodoStore} from '../Store'
+
 
 const Blog2 = () => {
+
+let [inpName,setInpName]=useState('')
+let [inpImage,setInpImage]=useState('')
+
+
+let [editName,setEditName]=useState('')
+let [editImage,setEditImage]=useState('')
+let [idx,setIdx]=useState(null)
+
+
+
+
+   let {users,get,Delete,addUser,Edit,ChangeStatus}=useTodoStore()
+  useEffect(()=>{
+get()
+  },[])
+
+  function addNewUser() {
+ let newUser={
+  name: inpName,
+  image: inpImage,
+}
+
+    addUser(newUser)
+    setInpName('')
+    setInpImage('')
+  }
+
+function openEdit(e) {
+  setEditImage(e.image)
+  setEditName(e.name)
+  setIdx(e.id)
+}
+
+
+function save() {
+  let updateUser={
+    name:editName,
+    image:editImage,
+    id:idx
+  }
+  Edit(idx,updateUser)
+  
+  setEditName('')
+  setEditImage('')
+}
+
+
+
+
+
   return (
-    <div>Blog2</div>
+    <div>
+      
+
+      <section className='w-full flex items-center gap-[20px] sm:gap-0  my-[30px] justify-between flex-col sm:flex-row sm:h-[318px]'>
+<article className='sm:w-[902px] p-[10px] sm:p-0 h-[196px] bg-[#1A1A1A] rounded-2xl sm:h-[316px] flex items-center justify-center'>
+<p className='sm:text-[48px] text-[24px] font-[600]'>Mastering the Art <span className=' text-black rounded-[50%] text-[20px] py-[5px] px-[10px] bg-[#CE7D63]'> →</span> <span className='text-[20px] text-[#CE7D63]'> 
+Start a Project</span> <br />
+of Minimalistic Design
+</p>
+</article>
+
+
+
+<article className='bg-[#1A1A1A] sm:w-[442px] p-[10px] h-[306px] flex flex-col items-center justify-around rounded-2xl sm:h-[318px]'>
+<div  className='bg-[#1F1F1F] sm:w-[422px] w-[338px] h-[64px] sm:h-[67px] rounded-2xl flex items-center justify-around'>
+<p className='text-[#B3B3B2]'>Author</p>
+<p className='text-[18px] font-[500]'>Sophia Roberts</p>
+</div>
+<div  className='bg-[#1F1F1F] sm:w-[422px] w-[338px] h-[64px] sm:h-[67px] rounded-2xl flex items-center justify-around'>
+<p className='text-[#B3B3B2]'>Published Date</p>
+<p className='text-[18px] font-[500]'>25th October 2023</p>
+</div>
+<div  className='bg-[#1F1F1F] sm:w-[422px] w-[338px] h-[64px] sm:h-[67px] rounded-2xl flex items-center justify-around'>
+<p className='text-[#B3B3B2]'>Category</p>
+<p className='text-[18px] font-[500]'>Design</p>
+</div>
+<div  className='bg-[#1F1F1F] sm:w-[422px] w-[338px] h-[64px] sm:h-[67px] rounded-2xl flex items-center justify-around'>
+<p className='text-[#B3B3B2]'>Read Time</p>
+<p className='text-[18px] font-[500]'>5 minutes</p>
+</div>
+</article>
+
+      </section>
+<img src={s5} alt="" />
+
+
+
+
+<section className='w-full sm:h-[1430px] sm:flex-row flex-col sm:gap-0 gap-[20px] py-[30px]  my-[30px] flex items-start justify-around'>
+  <img src={social} className='sm:block hidden' alt="" />
+
+<article className='sm:w-[843px] rounded-xl bg-[#1A1A1A] flex flex-col items-start justify-between p-[20px] sm:h-[1400px]'>
+<p className='text-[24px] font-[500]'>The Essence of Minimalism in Design</p>
+<p className='text-[#B3B3B2]'>In the realm of design, the essence of minimalism lies in the deliberate choice to distill complexity and convey a powerful message through simplicity. It's an art form that celebrates the beauty of space, emphasizing the significance of each carefully chosen element. A minimalist design isn't about deprivation; rather, it's a conscious decision to focus on the core, allowing the audience to engage with a visual narrative that is both refined and impactful. <br /><br />
+
+As we explore the essence of minimalistic design, we uncover the subtle nuances that contribute to its allure. The use of negative space, a hallmark of minimalism, creates breathing room within the composition, allowing the viewer's gaze to rest and appreciate the inherent beauty of the design. The simplicity in form and color becomes a canvas for expression, where every line and shade tells a story. By embracing minimalism, designers have the opportunity to communicate more with less, fostering a connection that transcends visual aesthetics.<br /><br />
+
+At its core, minimalism in design challenges conventional notions, encouraging a shift from excess to essence. It invites both creators and consumers to engage in a thoughtful dialogue with the visual elements, promoting a sense of mindfulness and intentionality. The essence of minimalistic design, therefore, lies not just in its visual appeal but in the profound impact it has on the way we perceive and interact with the world of design.</p>
+<p className='text-[24px] font-[500]'>Minimalism Beyond Aesthetics</p>
+
+<p className='text-[#B3B3B2]'>Beyond its visually captivating exterior, minimalism is a design philosophy that permeates every aspect of the creative process, extending its influence far beyond the surface. This philosophy becomes a lens through which designers view their craft, shaping not just what is seen but how it is experienced. The minimalist approach transcends mere aesthetics; it becomes a mindset that emphasizes clarity, functionality, and a deeper connection with the audience. <br /><br />
+
+Minimalism, as a philosophy, challenges the notion that complexity is synonymous with sophistication. It prompts designers to question the necessity of each element, encouraging a meticulous evaluation of form and function. This shift in perspective extends to the user experience, where the removal of unnecessary clutter allows for a seamless and intuitive interaction. Beyond creating visually pleasing designs, the minimalist philosophy becomes a guiding force for designing experiences that are inherently user-centric. <br /><br />
+</p>
+<p className='text-[24px] font-[500]'>Practical Tips for Mastering Minimalistic Design</p>
+<p className='text-[#B3B3B2]'>The journey to mastering minimalistic design involves a practical exploration of principles and techniques that breathe life into the philosophy. It begins with an understanding of the psychology of color in minimalism, where the strategic use of a limited color palette contributes to the overall impact of the design. The intentional choice of typography plays a pivotal role, guiding the viewer through the visual narrative with clarity and purpose. <br /><br />
+
+Navigating the terrain of minimalistic design also involves a keen awareness of the power of negative space. Far from being empty, this space becomes a deliberate pause, allowing the audience to absorb and appreciate the essential elements of the composition. Striking the right balance between simplicity and sophistication is an art in itself, requiring a discerning eye and a commitment to the core principles of minimalism. <br /><br />
+
+Practical tips extend beyond the theoretical, delving into the day-to-day decisions that shape a minimalist design. From the selection of imagery to the judicious use of graphic elements, each choice contributes to the overall impact. Mastery in minimalistic design is not merely about adherence to a set of rules; it's about developing an intuition that guides the creative process, ensuring that every design decision serves a purpose and contributes to the cohesive whole.</p>
+</article>
+
+
+<article className='sm:w-[325px] w-[357px] h-[260px] p-[20px] bg-[#1A1A1A] flex flex-col items-center justify-around rounded-xl sm:h-[275px]'>
+<div className='flex w-full items-center  gap-[15px]'>
+<img src={s6} alt="" />
+<div>
+  <p className='text-[18px] font-[500]' >Wade Warren</p>
+  <p className='text-[#81807E]'>Art Director</p>
+</div>
+</div>
+<p className='text-[#B3B3B2]'>Crafting visual narratives that captivate and inspire, weaving creativity into every chapter of the design journey.</p>
+
+<div className='bg-[#1F1F1F] w-[305px] h-[52px] rounded-xl flex items-center justify-around'>
+<p>🕊️</p>
+<p className='text-[#CCCCCC]'>@wadewarren</p>
+<p className='text-2xl'>↗</p>
+</div>
+
+</article>
+
+</section>
+
+
+
+
+
+
+<button className='border p-[5px] rounded-xl' onClick={addNewUser}>Add User</button> <br /><br />
+<input className='border p-[5px] rounded-xl' value={inpName} onChange={(e)=>setInpName(e.target.value)} type="text" placeholder='Name...' /> <br /><br />
+<input className='border rounded-xl p-[5px]' value={inpImage} onChange={(e)=>setInpImage(e.target.value)} type="text" placeholder='Image...' /> <br /><br />
+
+<input className='border p-[5px] rounded-xl' value={editName} onChange={(e)=>setEditName(e.target.value)} type="text" placeholder='edit Name...' /> <br /><br />
+<input className='border rounded-xl p-[5px]' value={editImage} onChange={(e)=>setEditImage(e.target.value)} type="text" placeholder='edit Image...' /> <br /><br />
+<button onClick={save} className='border p-[5px] rounded-xl bg-amber-500'>Update</button>
+    <section className='w-full sm:h-[565px] flex-col sm:flex-row h-[1431px] flex items-center justify-between my-[20px] '>
+
+{users.map((e)=>{
+  return (
+    <article key={e.id} className='sm:w-[430px] flex flex-col items-start justify-between sm:h-[565px] bg-[#1A1A1A] rounded-2xl p-[20px] '>
+      <img src={e.image} className='sm:w-[408px] w-[310px] h-[187px] sm:h-[243px] rounded-2xl' alt="" />
+      <p className='text-[20px] font-[500]'>{e.name}</p>
+<p className='text-[#B3B3B2]'>Mobile devices dominate digital interactions, making mobile user experience crucial for conversion rates. Explore mobile design best practices...</p>
+<div className='flex items-center gap-[20px]'>
+   <button className='border p-[5px] rounded-xl' onClick={()=>Delete(e.id)}>Delete</button>
+            <button className='border p-[5px] rounded-xl' onClick={()=>openEdit(e)}>Edit</button>
+</div>
+
+    </article>
+  )
+})}
+    
+
+
+
+    </section>
+
+
+
+
+    </div>
   )
 }
 
